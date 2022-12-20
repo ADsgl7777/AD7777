43a41504e6a832f8d28b9639fee313ad470c4d02
ADsgl7777 <121117042+ADsgl7777@users.noreply.github.com>
2022-12-20 17:03:40 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.Prefix = `;`;
+exports.Token = `MTA1NDcxODMxNjQ1Mjc4MjE4MA.Gg3pfR.1o5Ggs_F0daSzd4MDC9HtiHcNxdU6MyPVu2X24`;
+exports.Color = `#72AAFF`;
+exports.Port = `8080`;
+exports.BotName= `AGT OP`;
